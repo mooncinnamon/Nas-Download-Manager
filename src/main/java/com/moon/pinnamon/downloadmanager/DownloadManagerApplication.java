88100929335b7dae586434365752296f77e54a93
YuBin Moon <moon.pinnamon@gmail.com>
2021-02-07 15:42:57 +0900

    first commit

---
+++ src/main/java/com/moon/pinnamon/downloadmanager/DownloadManagerApplication.java
@@ -0,0 +1,13 @@
+package com.moon.pinnamon.downloadmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DownloadManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moon/pinnamon/downloadmanager/DownloadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.pinnamon.downloadmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DownloadManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
