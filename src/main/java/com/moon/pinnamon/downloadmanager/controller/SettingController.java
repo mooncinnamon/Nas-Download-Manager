8d8301556247a83115a64532ff0515d9deb8bd33
YuBin Moon <mooncinnamon@YuBinui-MacBookPro-2.local>
2021-03-17 00:10:43 +0900

    getSetting.http 파일 추가 작성

--- src/main/java/com/moon/pinnamon/downloadmanager/controller/SettingController.java
+++ src/main/java/com/moon/pinnamon/downloadmanager/controller/SettingController.java
@@ -3,6 +3,7 @@ package com.moon.pinnamon.downloadmanager.controller;
 import com.moon.pinnamon.downloadmanager.service.SettingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class SettingController {
@@ -14,7 +15,8 @@ public class SettingController {
     }
 
     @GetMapping("/v1/setting/video")
-    public String getVideoPath(){
-        return settingService.getVideoPath();
+    public Mono<String> getVideoPath() {
+        String url = settingService.getVideoPath();
+        return Mono.just(url);
     }
 }
